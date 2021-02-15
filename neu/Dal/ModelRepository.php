cb9bd075c4e6b70dafaac69825e143aa00c5ffd0
Jörn Neumeyer <contact@joern-neumeyer.de>
2021-02-15 22:48:47 +0100

    add first draft for model repository

--- neu/Dal/ModelRepository.php
+++ neu/Dal/ModelRepository.php
@@ -4,4 +4,34 @@
   namespace Neu\Dal;
 
   class ModelRepository {
+    public function __construct(
+      private string $defaultModel = ''
+    ) {
+    }
+
+    /**
+     * @param string $model
+     * @return ModelRepository
+     */
+    public function withDefaultModel(string $model) {
+      $this->defaultModel = $model;
+      return $this;
+    }
+
+    /**
+     * @param null $model
+     * @throws \Exception
+     */
+    public function load($model = null) {
+      if (!$model) {
+        $model = $this->defaultModel;
+      }
+      if (!$model) {
+        throw new \Exception('Missing model type!');
+      }
+    }
+
+    public function persist($instance) {
+
+    }
   }
