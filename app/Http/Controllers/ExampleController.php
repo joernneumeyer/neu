c414f6ee4a0db34a6d0d37baf8500f56998049f6
Jörn Neumeyer <contact@joern-neumeyer.de>
2021-02-15 22:55:06 +0100

    extend example

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -1,23 +1,27 @@
 <?php
+
   namespace App\Http\Controllers;
 
+  use App\Models\Address;
   use App\Models\SimpleUser;
+  use Neu\Annotations\Consumes;
   use Neu\Annotations\HandlerParameters\Body;
   use Neu\Annotations\HandlerParameters\Param;
   use Neu\Annotations\HandlerParameters\Query;
   use Neu\Annotations\Controller;
+  use Neu\Annotations\InjectUnique;
   use Neu\Annotations\Produces;
   use Neu\Annotations\Route;
   use Neu\Annotations\Status;
   use Neu\Dal\ModelRepository;
+  use Neu\Data\Reducers;
   use Neu\Http\ContentType;
   use Neu\Http\StatusCode;
 
-  #[Controller(path: '/moinsen')]
+  #[Controller]
   class ExampleController {
-    public function __construct(
-      private ModelRepository $repository
-    ) {
+    public function __construct(#[InjectUnique] private ModelRepository $repository) {
+      $repository->withDefaultModel(model: Address::class);
     }
 
     #[Route(method: 'GET', path: '/{username}')]
@@ -32,6 +36,7 @@
     #[Route(method: 'POST')]
     #[Status(code: StatusCode::Created)]
     #[Produces(contentType: ContentType::ApplicationXml)]
+    #[Consumes(contentType: ContentType::ApplicationJson)]
     public function example_post(#[Body] SimpleUser $user) {
       return $user;
     }
