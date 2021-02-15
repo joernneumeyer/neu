d4dcdc00312816bc5f46c618dcfb466ed366340f
Jörn Neumeyer <contact@joern-neumeyer.de>
2021-02-15 22:51:38 +0100

    add length validation for testing

--- app/Models/SimpleUser.php
+++ app/Models/SimpleUser.php
@@ -2,10 +2,12 @@
 
   namespace App\Models;
 
+  use Neu\Annotations\Length;
   use Neu\Annotations\ModelProperty;
 
   class SimpleUser {
     #[ModelProperty]
+    #[Length(min: 4)]
     public string $username = 'John';
     #[ModelProperty]
     private ?Address $address = null;
