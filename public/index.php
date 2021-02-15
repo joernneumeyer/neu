e4ed0680dba4e03e473aa6d0aca70ba8005c48d9
Jörn Neumeyer <contact@joern-neumeyer.de>
2021-02-15 22:45:56 +0100

    add better error reporting

--- public/index.php
+++ public/index.php
@@ -9,13 +9,21 @@
   require_once join(DIRECTORY_SEPARATOR, [__DIR__, '..', 'vendor', 'autoload.php']);
   define('APP_ROOT', dirname(__DIR__));
 
-  Neu::bootstrap();
-  $kernel = new Kernel();
-  $kernel->boot();
   try {
+    Neu::bootstrap();
+    $kernel = new Kernel();
+    $kernel->boot();
     $request = Request::from_global_state();
     $response = $kernel->processRequest($request);
   } catch (Throwable $e) {
-    $response = new Response(status: StatusCode::InternalServerError, body: $e);
+    try {
+      $response = niceResponseFromError($e);
+    } catch (Throwable $t) {
+      $response = (new Response(
+        status: 500,
+        body: 'Nice error reporting failed. You must have triggered a low-level/internal error! Please have a look at the following exception:'
+          . "\r\n\r\n" . $t
+      ))->contentType(\Neu\Http\ContentType::TextPlain);
+    }
   }
   $response->send();
