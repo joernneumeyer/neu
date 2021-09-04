296f55a2d897f00e4be1cc6d5b1babf1d6becd36
Jörn Neumeyer <contact@joern-neumeyer.de>
2021-09-04 22:30:28 +0200

    implement CORS middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,6 +1,19 @@
 <?php
 
+  namespace App\Http\Middleware;
 
-  class Cors {
+  use Neu\Http\PostMiddleware;
+  use Neu\Http\Response;
 
+  class Cors implements PostMiddleware {
+    public function __construct(
+      private Response $response
+    ) {
+    }
+
+    function apply(): void {
+      $this->response->headers['Access-Control-Allow-Origin'] = '*';
+      $this->response->headers['Access-Control-Allow-Headers'] = '*';
+      $this->response->headers['Access-Control-Allow-Method'] = '*';
+    }
   }
