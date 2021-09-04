c7ae26b29a3582f656d91d29ae5cdd3f3cefae13
Jörn Neumeyer <contact@joern-neumeyer.de>
2021-09-04 22:49:19 +0200

    update example controller

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -1,43 +1,42 @@
 <?php
 
+
   namespace App\Http\Controllers;
 
-  use App\Models\Address;
-  use App\Models\SimpleUser;
+
+  use App\Http\Middleware\Cors;
   use Neu\Annotations\Consumes;
-  use Neu\Annotations\HandlerParameters\Body;
-  use Neu\Annotations\HandlerParameters\Param;
-  use Neu\Annotations\HandlerParameters\Query;
   use Neu\Annotations\Controller;
-  use Neu\Annotations\InjectUnique;
   use Neu\Annotations\Produces;
   use Neu\Annotations\Route;
-  use Neu\Annotations\Status;
-  use Neu\Dal\ModelRepository;
-  use Neu\Data\Reducers;
+  use Neu\Annotations\UseMiddleware;
   use Neu\Http\ContentType;
-  use Neu\Http\StatusCode;
+  use Neu\Http\Request;
 
   #[Controller]
   class ExampleController {
-    public function __construct(#[InjectUnique] private ModelRepository $repository) {
-      $repository->withDefaultModel(model: Address::class);
+    public function __construct(
+      private Request $request
+    ) {
     }
 
-    #[Route(method: 'GET', path: '/{username}')]
-    public function example(#[Param] string $username, #[Query] ?int $bar) {
-      $response = "Hello, $username!";
-      if (!is_null($bar)) {
-        $response .= " I'm at this bar $bar.";
-      }
-      return $response;
-    }
-
+    /**
+     * @return int[]
+     */
     #[Route(method: 'POST')]
-    #[Status(code: StatusCode::Created)]
-    #[Produces(contentType: ContentType::ApplicationXml)]
-    #[Consumes(contentType: ContentType::ApplicationJson)]
-    public function example_post(#[Body] SimpleUser $user) {
-      return $user;
+    #[Produces(ContentType::ApplicationJson)]
+    #[Consumes(ContentType::ApplicationJson)]
+    public function hello() {
+      return [1,2,3];
+    }
+
+    /**
+     *
+     */
+    #[Route(method: 'GET')]
+    #[Produces(contentType: ContentType::TextPlain)]
+    #[UseMiddleware(name: Cors::class)]
+    public function someRandomHandler() {
+      return 'response from handler!';
     }
   }
